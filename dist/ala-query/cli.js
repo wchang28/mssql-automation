576f93d039355d70af3b0daf1f84ae6666094e9b
wchang28 <wchang28@hotmail.com>
2020-05-02 01:13:01 -0400

    add ala-query

---
+++ dist/ala-query/cli.js
@@ -0,0 +1,20 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var fs = require("fs");
+var alasql = require("alasql");
+var query = process.argv[2];
+if (!query) {
+    console.error("query is missing");
+    process.exit(1);
+}
+try {
+    var json = fs.readFileSync(0, "utf-8");
+    var data = JSON.parse(json);
+    var ret = alasql(query, [data]);
+    process.stdout.write(JSON.stringify(ret));
+}
+catch (e) {
+    console.error(e);
+    process.exit(1);
+}
+//# sourceMappingURL=cli.js.map
---
+++ src/ala-query/cli.ts
@@ -0,0 +1,18 @@
+import * as fs from "fs";
+const alasql = require("alasql");
+
+const query = process.argv[2];
+if (!query) {
+    console.error("query is missing");
+    process.exit(1);
+}
+
+try {
+    const json = fs.readFileSync(0, "utf-8");
+    const data = JSON.parse(json);
+    const ret = alasql(query, [data]);
+    process.stdout.write(JSON.stringify(ret));
+} catch (e) {
+    console.error(e);
+    process.exit(1);
+}
